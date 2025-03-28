5ea00772de1e87f72e9c5f8a420f68decb96c9d1
GwerhDev <gwerh.dev@gmail.com>
2025-03-28 09:45:42 -0300

    deleting auth endpoint

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -1,8 +1,34 @@
 const router = require("express").Router();
-const { User } = require("../models/User");
 const { message } = require("../messages");
 const { decodeToken } = require("../integrations/jwt");
 const bcrypt = require("bcrypt");
+const userSchema = require("../models/User");
+
+router.get("/", async(req, res) => {
+  try {
+    const userToken = req.headers.authorization || null;
+    if(!userToken) return res.status(401).send({ logged: false, message: message.user.unauthorized });
+
+    const decodedToken = await decodeToken(userToken);
+    const user = await userSchema.findById(decodedToken.data._id);
+    
+    if(!user) return res.status(404).send({ logged: false, message: message.user.notfound });
+    
+    const userData = {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      isVerified: user.isVerified,
+      role: user.role,
+      profilePic: user.profilePic || user.googlePic
+    };
+    
+    return res.status(200).send({ logged: true, userData });
+    
+  } catch (error) {
+    return res.status(500).send({ error: message.user.error })
+  }
+});
 
 router.patch("/update/:id", async (req, res) => {
   const { id } = req.params;
@@ -11,17 +37,17 @@ router.patch("/update/:id", async (req, res) => {
   if(!userToken) return res.status(403).json({ message: message.admin.permissionDenied });
   
   const decodedToken = await decodeToken(userToken);
-  if(decodedToken.data.id !== id) return res.status(403).json({ message: message.admin.permissionDenied });
+  if(decodedToken.data._id !== id) return res.status(403).json({ message: message.admin.permissionDenied });
   
   try {
-    const existingUser = await User.findByPk(id);
+    const existingUser = await userSchema.findById(id);
     if (!existingUser) return res.status(404).json({ message: message.admin.updateuser.failure });
     
     const salt = await bcrypt.genSalt();
     
     if(body?.password) body.password = await bcrypt.hash(body.password, salt);
         
-    await User.update(body, { where: { id } });
+    await userSchema.update(body, { where: { id } });
     
     return res.status(200).json({ message: message.admin.updateuser.success });
   } catch (error) {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-const auth = require('../controllers/auth');
 const logout = require('../controllers/logout');
 const session = require('../controllers/session');
 const account = require('../controllers/account');
@@ -11,7 +10,6 @@ const googlLogin = require('../controllers/login-google');
 const innerSignup = require('../controllers/signup-inner');
 const googleSignup = require('../controllers/signup-google');
 
-router.use("/auth", auth);
 router.use("/logout", logout);
 router.use("/session", session);
 router.use("/account", account);

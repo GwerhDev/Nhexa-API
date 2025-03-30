56edb3f869cb638d548055b81297b57afd83ce28
GwerhDev <gwerh.dev@gmail.com>
2025-03-30 00:02:58 -0300

    now tokens are taken from cookies for fetchings

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -6,7 +6,7 @@ const userSchema = require("../models/User");
 
 router.get("/", async (req, res) => {
   try {
-    const userToken = req.headers.authorization || null;
+    const { userToken } = req.cookies || {};
     if (!userToken) return res.status(401).send({ logged: false, message: message.user.unauthorized });
 
     const decodedToken = await decodeToken(userToken);

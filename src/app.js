80b674e7bd15914615566aaf82c72750348732b4
Gwerh <gwerh@kali.Gwerh>
2025-04-12 21:04:18 -0400

    adding streamby core

--- src/app.js
+++ src/app.js
@@ -9,11 +9,34 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const { privateSecret, allwedOrigins } = require("./config");
 const cookieParser = require('cookie-parser');
+const { createStreamByRouter } = require("streamby-core");
+const { decodeToken } = require("./integrations/jwt");
 
 server.use(bodyParser.json({ limit: '100mb' }));
 server.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+
+server.use('/streamby', createStreamByRouter({
+  storageProvider: {
+    type: 's3',
+    config: {
+      bucket: process.env.S3_BUCKET,
+      region: process.env.S3_REGION,
+      accessKeyId: process.env.S3_KEY,
+      secretAccessKey: process.env.S3_SECRET
+    }
+  },
+  authProvider: async (req) => {
+    const token = req.cookies['auth_token'] || req.headers.authorization?.split(' ')[1];
+    const user = decodeToken(token);
+    return {
+      userId: user.id,
+      projectId: user.projectId,
+      role: user.role,
+    };
+  }
+}));
 
 server.use((req, res, next) => {
   console.log('request from:', req.headers.origin);

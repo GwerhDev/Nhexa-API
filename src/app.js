6b9535c6c9816ad889e8d2abaeb94994ce36e57a
GwerhDev <gwerh.dev@gmail.com>
2025-04-15 08:35:42 -0400

    upgrading version  of streamby

--- src/app.js
+++ src/app.js
@@ -19,6 +19,34 @@ server.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 
+server.use((req, res, next) => {
+  console.log('request from:', req.headers.origin);
+  console.log('method:', req.method);
+  console.log('route:', req.url);
+
+  const origin = req.headers.origin;
+
+  if (allwedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  };
+
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+server.use(session({
+  secret: privateSecret,
+  resave: false,
+  saveUninitialized: false
+}));
+
 server.use('/streamby', createStreamByRouter({
   storageProvider: {
     type: 's3',
@@ -55,35 +83,6 @@ server.use('/streamby', createStreamByRouter({
       }
     };
   }
-}));
-
-
-server.use((req, res, next) => {
-  console.log('request from:', req.headers.origin);
-  console.log('method:', req.method);
-  console.log('route:', req.url);
-
-  const origin = req.headers.origin;
-
-  if (allwedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  };
-
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin');
-
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
-
-server.use(session({
-  secret: privateSecret,
-  resave: false,
-  saveUninitialized: false
 }));
 
 server.use(passport.initialize());

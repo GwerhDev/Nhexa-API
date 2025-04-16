12abe67563c32014d6b35137fc3124056eafe918
GwerhDev <gwerh.dev@gmail.com>
2025-04-16 19:57:39 -0400

    adding environment for login

--- src/controllers/logout.js
+++ src/controllers/logout.js
@@ -1,15 +1,26 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.clearCookie("userToken", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    domain: ".nhexa.cl",
-    path: "/",
-  });
+
+  if (process.env.NODE_ENV === production) {
+    res.clearCookie("userToken", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      domain: ".nhexa.cl",
+      path: "/",
+    });
+  } else {
+    res.clearCookie("userToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "Lax",
+      path: "/",
+    });
+  }
 
   res.status(200).json({ loggedOut: true });
 });
+
 
 module.exports = router;

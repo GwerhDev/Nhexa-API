e866d651a0c00485d1aa860bbf5a9b1dd24ac013
GwerhDev <gwerh.dev@gmail.com>
2025-07-12 16:05:49 -0400

    changing applist

--- src/controllers/app-list.js
+++ src/controllers/app-list.js
@@ -6,13 +6,13 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   const userToken = req.cookies['userToken'] || req.headers.authorization?.split(' ')[1];
-  if (!userToken) return res.status(200).send(appList.user);
+  if (!userToken) return res.status(200).send({ user: appList.user });
 
   const decoded = await decodeToken(userToken) || null;
   const { role } = decoded.data || {};
 
-  if (role === roles.admin) return res.status(200).send(appList.admin);
-  return res.status(200).send(appList.user);
+  if (role === roles.admin) return res.status(200).send(appList);
+  return res.status(200).send({ user: appList.user });
 });
 
 module.exports = router;
--- src/misc/app-list.js
+++ src/misc/app-list.js
@@ -25,28 +25,10 @@ export const appList = {
   admin: [
     {
       url: "https://streamby.nhexa.cl",
-      label: "Streamby",
+      label: "Projects",
       route: "#",
       description: "Gestor de contenido",
       icon: "https://streamby.s3.sa-east-1.amazonaws.com/6826af8fe0e42c69e2fa9d1d%2Fproject-image"
-    }, {
-      url: "https://spectra.nhexa.cl",
-      label: "Spectra",
-      route: "#",
-      description: "Streaming",
-      icon: "https://streamby.s3.sa-east-1.amazonaws.com/6826ade224186d06a0683b2d%2Fproject-image"
-    }, {
-      url: "https://spellcast.nhexa.cl",
-      label: "Spellcast",
-      route: "#",
-      description: "Audio y música",
-      icon: null
-    }, {
-      url: "https://havenstore.nhexa.cl",
-      label: "Havenstore",
-      description: "Tienda",
-      route: "#",
-      icon: null
-    }
+    },
   ]
 };

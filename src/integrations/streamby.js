d6252ace0c11e37c1c2e9ffd32af2fe6e960bac2
gerardo <gerardo@recemed.com>
2025-07-23 11:58:40 -0400

    updating

--- src/integrations/streamby.js
+++ src/integrations/streamby.js
@@ -1,11 +1,13 @@
 const { decodeToken } = require("./jwt");
 const { createStreamByRouter } = require("streamby-core");
 const { awsBucket, awsBucketRegion, awsAccessKey, awsSecretKey, mongodbString, supabaseString } = require("../config");
+const { prisma } = require("./prisma");
 
 const authProvider = async (req) => {
   const userToken = req.cookies['userToken'] || req.headers.authorization?.split(' ')?.[1];
   const decoded = await decodeToken(userToken);
   const user = await prisma.users.findUnique({ where: { id: decoded.data.id } });
+  console.log(user);
 
   const authObject = {
     role: user.role,
